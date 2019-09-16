a933304101283d97c80e7fdd06f1dbee73823f92
detikorg <55391026+detikorg@users.noreply.github.com>
2019-09-16 22:26:46 +0700

    Update api.php

--- api.php
+++ api.php
@@ -34,17 +34,8 @@ class jsonp{
             return $return;
     }
     public function connectApi($params){
-		$callback['respon']=$this->makeRequest($params);
+		$callback=$this->makeRequest($params);
 		return $callback;
 	}
 	
 }
-
-$params['user']		='user@xabber.org';
-$params['password']	='12345';
-$params['kepada']	='user-tujuan@xabber.org';
-$params['pesan']	='Test API XMPP Jabber dengan PHP';
-$params['mod']		='xmppsender';
-$jsonp=new jsonp();
-$respon=$jsonp->connectApi($params);
-echo '<pre>'.print_r($respon,true).'</pre>';
